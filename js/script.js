402c5d3fa48bb35872c68970219750638477767e
YacoubAlhaidari <Yacoub.haid@cloudszone.tech>
2023-12-21 20:29:31 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,10 @@
+AOS.init();
+// start sliders 
+var elem = document.querySelector('#main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'right',
+  contain: true
+});
+
+// ---------------------------------------
